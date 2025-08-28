50ff8367348383b29b6c92b6cf40b34c60d5e088
Aman Iqbal <amaaniqbal0011@gmail.com>
2025-08-29 00:44:33 +0500

    controlling tool behaviour using different methods

--- agents-tools/function-calling-tool/main.py
+++ agents-tools/function-calling-tool/main.py
@@ -1,5 +1,6 @@
 from decouple import config
-from agents import AsyncOpenAI,OpenAIChatCompletionsModel,Agent,RunConfig,Runner,set_tracing_disabled
+from agents import AsyncOpenAI,OpenAIChatCompletionsModel,Agent,RunConfig,Runner,set_tracing_disabled,ModelSettings
+from agents.agent import StopAtTools
 from agents import function_tool,enable_verbose_stdout_logging
 from user_data import fetch_user_data,fetch_user_data_by_id
 set_tracing_disabled(True)
@@ -11,13 +12,13 @@ base_url = config('gemini_base_url')
 def plus(a: int, b: int) -> str:
     """This is plus function that adds two numbers."""
     print("Plus function called")
-    return f"The sum of {a} and {b} is {a + b}"
+    return f"The sum of {a} and {b} is {a + b} and my answer is {a + b}"
 
 @function_tool
 async def subtract(a: int, b: int) -> str:
     """This is plus function that subtract two numbers."""
     print("Subtract function called")
-    return f"The sum of {a} and {b} is {a - b}"
+    return f"The sum of {a} and {b} is {a - b} and my answer is {a - b}"
 
 
 
@@ -33,12 +34,16 @@ agent = Agent(
     name="Gemini Agent",
     instructions="You are a helpful assistant that answer my every question using LLm or tools.",
     tools=[plus, subtract, fetch_user_data, fetch_user_data_by_id],
+    tool_use_behavior=StopAtTools(stop_at_tool_names=["plus","subtract"]),
+    model_settings=ModelSettings(tool_choice="subtract",parallel_tool_calls=False),
+    reset_tool_choice=True
     )
 
 res = Runner.run_sync(
     starting_agent=agent, 
-    input="capital of France? ",
-    run_config = RunConfig(model=gemini_model,model_provider=client)
+    input="2+2-2+4+5-2",
+    run_config = RunConfig(model=gemini_model,model_provider=client),
+    max_turns=5
 )
 
 print(res.final_output)
