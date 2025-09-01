6236d749d826c338202f2bd941ededb33f5dafb7
Aman Iqbal <amaaniqbal0011@gmail.com>
2025-09-02 03:16:23 +0500

    ...

--- projects/hotel-Agent/hotel_agent/data/hotel_data.py
+++ projects/hotel-Agent/hotel_agent/data/hotel_data.py
@@ -8,8 +8,6 @@ def res_info():
         "total_rooms": 200,
         "private_room": True,
         "number_of_private_rooms": 50,
-        "numbers_of_room_available" : 150,
-        "accounts":5,
-        "tax":"Monthly paid 2500$"
+        "numbers_of_room_available" : 150
     }
-  return f'Our hotel {data['hotel_name']} has {data['total_rooms']} rooms, with {data['numbers_of_room_available']} available. and private rooms is {data["private_room"]} and number of private rooms is {data["number_of_private_rooms"]} and accounts is{data["accounts"]} and we pay tax{data["tax"]}'
+  return f'Our hotel {data['hotel_name']} has {data['total_rooms']} rooms, with {data['numbers_of_room_available']} available. and private rooms is {data["private_room"]} and number of private rooms is {data["number_of_private_rooms"]}'
--- projects/hotel-Agent/hotel_agent/main.py
+++ projects/hotel-Agent/hotel_agent/main.py
@@ -1,61 +1,20 @@
 from decouple import config
-from agents import AsyncOpenAI,OpenAIChatCompletionsModel,Agent,Runner,set_tracing_disabled,RunContextWrapper,GuardrailFunctionOutput,OutputGuardrailTripwireTriggered,input_guardrail,InputGuardrailTripwireTriggered,output_guardrail,TResponseInputItem
+from agents import AsyncOpenAI,OpenAIChatCompletionsModel,Agent,Runner,function_tool,set_tracing_export_api_key,trace
 from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX
 from data.hotel_data import res_info
-from pydantic import BaseModel
 
-set_tracing_disabled(True)
 
+
+
+
+openai_key = config("OPEN_API_KEY")
 key = config('GEMINI_API_KEY')
 base_url = config('gemini_base_url')
 
-
-
-class MyDataOutput(BaseModel):
-    is_hotel_query: bool
-    is_hotel_account_or_tax_query:bool
-    reason:str
+set_tracing_export_api_key(openai_key)
 
 client = AsyncOpenAI(api_key=key,base_url=base_url)
 MODEL = OpenAIChatCompletionsModel(model="gemini-2.5-flash",openai_client=client)
-
-
-# input guardrail
-@input_guardrail
-async def guardrail_input_function(ctx:RunContextWrapper[None],agent:Agent,input: str | list[TResponseInputItem]) -> GuardrailFunctionOutput:
-    
-    res = await Runner.run(
-        guardrail_agent,
-        input=input,
-        context=ctx.context
-    )
-    return GuardrailFunctionOutput(
-        output_info=res.final_output,
-        tripwire_triggered=not res.final_output.is_hotel_query
-    )
-    
-    
-@output_guardrail
-async def guardrail_output_function(ctx:RunContextWrapper[None],agent:Agent,input:str | list[TResponseInputItem] ):
-    
-    res = await Runner.run(
-        guardrail_agent,
-        input=input,
-        context=ctx.context
-    )
-    return GuardrailFunctionOutput(
-        output_info=res.final_output,
-        tripwire_triggered=res.final_output.is_hotel_account_or_tax_query
-    )    
-
-    
-guardrail_agent = Agent( 
-    name = "Guardrail Agent",
-    instructions='Check hotel related queries.and account or tax related query',
-    model=MODEL,
-    output_type=MyDataOutput
-   
-)
 
 hotel_assistant = Agent(
     name="Hotel Customer Care Assistant",
@@ -64,31 +23,29 @@ hotel_assistant = Agent(
    If the tool gives structured data, extract the relevant information and explain it naturally.""",
     handoff_description="Helpful Hotel Assistant",
     model=MODEL,
-    tools=[res_info],
-    input_guardrails=[guardrail_input_function],
-    output_guardrails=[guardrail_output_function]
-   
+    tools=[res_info]
    
 )
 
+agent = Agent(
+    name="assistant",
+    instructions='You are a helpful assistant that can hand off hotel questions to the hotel agent',
+    model=MODEL,
+    handoffs=[hotel_assistant]
+   
+)
 
 while True:
     prompt = input("Enter your prompt: ")
     
     if prompt == "exit":
         break
-    try:
     
-        res=Runner.run_sync(
-        starting_agent=hotel_assistant,
-        input=prompt,
-    ) 
-        print(res.final_output)
-    except InputGuardrailTripwireTriggered as e:
-        print ("Please ask hotel related questions")   
-        
-    except OutputGuardrailTripwireTriggered as e:
-        print("I am not able to provide personal info")    
+    res=Runner.run_sync(
+    starting_agent=agent,
+    input=prompt,
+) 
+    print(res.final_output)
     
 
 
